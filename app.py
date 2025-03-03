eea5c901c069eafbaee4b631fbf97bcf2bf9012f
Irum Naureen <57554407+irum13@users.noreply.github.com>
2025-03-04 03:18:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -136,3 +136,5 @@ def notebook():
 
 if __name__ == "__main__":
     app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))  # Use the port provided by Render
+    app.run(host="0.0.0.0", port=port, debug=True)
